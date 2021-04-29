fed9291cd8f5196aadf5dfefa039165a441e33e6
Viskhan <rustam1987@mail.ru>
2021-04-29 21:15:05 +0300

    sent DZ1

---
+++ DZ-1/DZ-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Now.ToShortDateString()}");
+        }
+    }
+}
