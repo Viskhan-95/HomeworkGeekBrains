3c262332f26cfc62617aff2de8969b8b4735edab
Viskhan <rustam1987@mail.ru>
2021-04-29 20:48:32 +0300

    sent dz 1

---
+++ DZ-1/DZ-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DZ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userName}, today {DateTime.Now.ToShortDateString()}.");
+        }
+    }
+}
